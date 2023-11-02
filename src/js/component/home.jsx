89a239f58b784a6ea2bff675322ce09f1340c765
Marawan Elzoeiry <140724136+MarawanElzoeiry@users.noreply.github.com>
2023-11-02 00:45:12 +0000

    Semi-finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
 import React from "react";
+import { useState } from "react";
+import "./App.css"; 
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+
+function TodoItem ({label, is_done, delete_todo, toggle_todo }) { 
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todo-item ">
+				<input type="checkbox" checked = {is_done} onChange={toggle_todo}/>
+				<span className="todo-text">{label}</span>
+				<button className="btn btn-danger" onClick={delete_todo}>Cross out</button>
 		</div>
+	)
+}
+
+
+function App () {
+const [todos, setTodos] = useState([
+	{label: "Play Video Games", is_done: false},
+	{label: "Run for 5 miles", is_done:false}
+]); 
+
+
+
+
+	return (
+		<form  className="container d-flex flex-column align-items-center justify-content-start">
+			<h1>Todo List</h1>
+			<input className="form-control form-control-lg" type="text" placeholder="Enter your tasks here" aria-label="input field"></input>
+		
+		{todos.map((item) => (
+			<TodoItem label = {item.label} 
+			is_done={item.is_done}
+			toggle_todo = {()=>
+			setTodos (
+			todos.toSpliced (idx , 1, {
+				label: item.label,	
+				is_done: !item.is_done,
+			})
+			)
+			} 
+			/>
+))}
+		</form>
 	);
 };
 
-export default Home;
+export default App;
+
+
+
+
